441df963e24f8fa98397d63cd493226fe309285b
Daeki <kimdaeki@gmail.com>
2022-01-14 10:28:24 +0900

    update View

--- src/com/iu/s1/human/HumanView.java
+++ src/com/iu/s1/human/HumanView.java
@@ -6,6 +6,19 @@ public class HumanView {
 	
 	//한사람의 정보를 출력하는 메서드
 	//정보가 있으면 출력, 없으면 정보가 없음 출력
+	public void view(HumanDTO humanDTO) {
+		
+		if(humanDTO != null) {
+			System.out.println("Name : "+humanDTO.getName());
+			System.out.println("Nick : "+humanDTO.getNick());
+			System.out.println("Email : "+humanDTO.getEmail());
+			System.out.println("Phone : "+humanDTO.getPhone());
+			System.out.println("Birth : "+humanDTO.getBirth());
+		}else {
+			System.out.println("정보가 없다");
+		}
+		
+	}
 	
 	
 	public void view(List<HumanDTO> ar) {
